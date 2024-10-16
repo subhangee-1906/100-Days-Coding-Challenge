5a92e913a5e8369701ea04b0586dc0b741801417
Subhangee Sahoo <shubh190601@gmail.com>
2024-10-16 20:24:23 +0530

    Simple Array Sum

---
+++ Day9/Solution_.java
@@ -0,0 +1,37 @@
+package Day9;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    public static int simpleArraySum(List<Integer> ar) {
+        int sum = 0;
+        for (int num : ar) {
+            sum += num;
+        }
+        return sum;
+    }
+}
+
+public class Solution_ {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.simpleArraySum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
