1fe1f61ddd7aa202bbd250a70c91fa635bcbe795
Subhangee Sahoo <shubh190601@gmail.com>
2024-10-19 19:15:31 +0530

    Merge two sorted lists

---
+++ Day12/Solution_.java
@@ -0,0 +1,49 @@
+package Day12;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<Integer> arr) {
+    // Write your code here
+        long totalSum = 0;
+        int minElement = Integer.MAX_VALUE;
+        int maxElement = Integer.MIN_VALUE;
+        for (int num : arr) {
+            totalSum += num;
+            if (num < minElement) {
+                minElement = num;
+            }
+            if (num > maxElement) {
+                maxElement = num;
+            }
+        }
+        long minSum = totalSum - maxElement;
+        long maxSum = totalSum - minElement;
+        System.out.println(minSum + " " + maxSum);
+    }
+
+}
+
+public class Solution_ {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        Result.miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
