18649477f829613264fa771d1cf31da8e8cd455f
Subhangee Sahoo <shubh190601@gmail.com>
2024-10-31 10:49:23 +0530

    Plus One

---
+++ Day24/Solution_.java
@@ -0,0 +1,23 @@
+package Day24;
+
+class Solution {
+    public int[] plusOne(int[] digits) 
+    {
+        for (int i = digits.length - 1; i >= 0; i--)
+        {
+            digits[i]++;
+            if (digits[i] == 10)
+            {
+                digits[i] = 0;
+            } 
+            else 
+            {
+                return digits;
+            }
+        }
+        int[] newDigits = new int[digits.length + 1];
+        newDigits[0] = 1; 
+        return newDigits;
+    }
+}
+
