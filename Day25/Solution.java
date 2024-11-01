33d132622f48f68d60d9f144af5f959353060462
Subhangee Sahoo <shubh190601@gmail.com>
2024-11-01 14:08:05 +0530

    Camel Case

---
+++ Day25/Solution.java
@@ -0,0 +1,42 @@
+package Day25;
+
+import java.io.*;
+
+class Result {
+
+    /*
+     * Complete the 'camelcase' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int camelcase(String s) {
+        int wordCount = 1; 
+        for (char ch : s.toCharArray()) {
+            if (Character.isUpperCase(ch)) {
+                wordCount++;
+            }
+        }
+        return wordCount;
+    }
+}
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        int result = Result.camelcase(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
