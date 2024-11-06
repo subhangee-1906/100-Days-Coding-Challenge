4b93fc84b7048c615079962ab4aba4b1934c74f3
Subhangee Sahoo <shubh190601@gmail.com>
2024-11-06 22:09:48 +0530

    Funny String

---
+++ Day30/Solution.java
@@ -0,0 +1,63 @@
+package Day30;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'funnyString' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String funnyString(String s) {
+    int n = s.length();
+    
+    for (int i = 1; i < n; i++) {
+        int originalDiff = Math.abs(s.charAt(i) - s.charAt(i - 1));
+        int reversedDiff = Math.abs(s.charAt(n - i) - s.charAt(n - i - 1));
+        
+        if (originalDiff != reversedDiff) {
+            return "Not Funny";
+        }
+    }
+    return "Funny";
+}
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                String result = Result.funnyString(s);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
