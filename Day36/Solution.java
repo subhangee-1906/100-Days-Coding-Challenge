15cd8a6a12625313bb6b932c879bfa286817d943
Subhangee Sahoo <shubh190601@gmail.com>
2024-11-24 12:12:48 +0530

    Length of Last Word

---
+++ Day36/Solution.java
@@ -0,0 +1,10 @@
+package Day36;
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+        int lastSpaceIndex = s.lastIndexOf(' ');
+        return s.length() - lastSpaceIndex - 1;
+    }
+}
+
