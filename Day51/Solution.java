c13a59b41caa45ec1b2e59025b4d42cceae2b742
Subhangee Sahoo <shubh190601@gmail.com>
2024-12-09 19:48:01 +0530

    Single number

---
+++ Day51/Solution.java
@@ -0,0 +1,12 @@
+package Day51;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num; 
+        }
+        return result;
+    }
+}
+
